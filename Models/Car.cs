eadda8b6afab905f06f2c71048210fb351d1100c
Alex Luciano <alex.luciano.topan@gmail.com>
2025-04-21 19:00:24 -0300

    Iniciando o projeto. Adicionando o model de Car.

---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+namespace CarSeller.Models;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Image { get; set; }
+    public double Price { get; set; }
+}
